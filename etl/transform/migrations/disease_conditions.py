5c9dc720a948a99249876e0c95fc945857a9f9cc
Péter Hanich <hanipet@gmail.com>
2025-05-13 19:36:52 +0200

    feat: add disease conditions table

---
+++ etl/transform/migrations/disease_conditions.py
@@ -0,0 +1,34 @@
+# %% tags=["parameters"]
+upstream = ['extract']
+product: list[str] | None = None
+extract_path: str | None = None
+
+# %%
+from pandas import DataFrame
+
+from etl.transform.utils.utils import save_to_csv, load_csv
+
+
+def create_disease_conditions_table(disease: DataFrame) -> DataFrame:
+    # Extract column names after the first five columns
+    conditions_columns = disease.columns[5:]
+    # Sort the column names
+    sorted_conditions_columns = sorted(conditions_columns)
+
+    # Format each column name: capitalize the first word and replace underscores with spaces
+    formatted_conditions = [
+        col.replace('_', ' ').capitalize() for col in conditions_columns
+    ]
+
+    # Create the Disease Conditions table
+    disease_conditions_table = [{"id": idx + 1, "name": col} for idx, col in enumerate(formatted_conditions)]
+
+    # Convert to DataFrame
+    disease_conditions_df = DataFrame(disease_conditions_table)
+    return disease_conditions_df
+
+
+disease_path = extract_path + "/disease/disease.csv"
+disease_df = load_csv(disease_path)
+df: DataFrame = create_disease_conditions_table(disease_df)
+save_to_csv(df, product['data'])
