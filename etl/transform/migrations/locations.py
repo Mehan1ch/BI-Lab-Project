ddadc515e66281a58d9ac0fc78ab068b85e83222
Péter Hanich <hanipet@gmail.com>
2025-05-13 20:24:11 +0200

    fix: locations are unique

--- etl/transform/migrations/locations.py
+++ etl/transform/migrations/locations.py
@@ -12,19 +12,25 @@ from etl.transform.utils.utils import load_csv, save_to_csv
 
 
 def create_locations_table(weather: DataFrame, timezones: DataFrame, countries: DataFrame) -> DataFrame:
-    # Merge weather data with countries and timezones to get the required fields
+    # Use a set to ensure unique locations
+    unique_locations = set()
     locations_data = []
+
     for idx, row in weather.iterrows():
         country_id = countries[countries['name'] == row['country']]['id'].values[0]
         timezone_id = timezones[timezones['timezone'] == row['timezone']]['id'].values[0]
-        locations_data.append({
-            "id": idx + 1,
-            "location_name": row['location_name'],
-            "latitude": row['latitude'],
-            "longitude": row['longitude'],
-            "timezone_id": timezone_id,
-            "country_id": country_id
-        })
+        location_tuple = (row['location_name'], row['latitude'], row['longitude'], timezone_id, country_id)
+
+        if location_tuple not in unique_locations:
+            unique_locations.add(location_tuple)
+            locations_data.append({
+                "id": len(locations_data) + 1,
+                "location_name": row['location_name'],
+                "latitude": row['latitude'],
+                "longitude": row['longitude'],
+                "timezone_id": timezone_id,
+                "country_id": country_id
+            })
 
     # Create a DataFrame for the Locations table
     locations_df = DataFrame(locations_data)
