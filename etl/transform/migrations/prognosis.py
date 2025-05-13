31f34e2f5b57b1b99254d6dcbacbf639f67c1311
Péter Hanich <hanipet@gmail.com>
2025-05-13 17:51:10 +0200

    feat: create prognosis table

---
+++ etl/transform/migrations/prognosis.py
@@ -0,0 +1,28 @@
+# %% tags=["parameters"]
+upstream = ['extract']
+product: list[str] | None = None
+extract_path: str | None = None
+transform_path: str | None = None
+
+# %%
+from pandas import DataFrame
+
+from etl.transform.utils.utils import save_to_csv, load_csv
+
+
+def create_prognosis_table(disease: DataFrame) -> DataFrame:
+    # Extract unique names from the disease_df
+    unique_names = disease['prognosis'].unique()
+
+    # Create the prognosis table
+    prognosis_table = [{"id": idx + 1, "name": name} for idx, name in enumerate(unique_names)]
+
+    # Convert to DataFrame
+    prognosis_df = DataFrame(prognosis_table)
+    return prognosis_df
+
+
+disease_path = extract_path + "/disease/disease.csv"
+disease_df = load_csv(disease_path)
+df: DataFrame = create_prognosis_table(disease_df)
+save_to_csv(df, product['data'])
