9ebba6b363906b9eb6ed073e8049bc19b1bface9
Péter Hanich <hanipet@gmail.com>
2025-05-14 12:16:43 +0200

    feat: implement locations model

---
+++ etl/load/models/locations.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Diagnosis(Base):
+    __tablename__ = 'diagnoses'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    timezone_id = Column(Integer, ForeignKey('timezones.id'), nullable=False)
+    country_id = Column(Integer, ForeignKey('countries.id'), nullable=False)
