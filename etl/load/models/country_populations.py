d3f943f5cc0b7d226b2f69a29630dabae6db6b6a
Péter Hanich <hanipet@gmail.com>
2025-05-14 12:01:39 +0200

    feat: implement country_populations model

---
+++ etl/load/models/country_populations.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class CountryPopulation(Base):
+    __tablename__ = 'country_populations'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    country_id = Column(Integer, ForeignKey('countries.id'), nullable=False)
+    age_group_id = Column(Integer, ForeignKey('age_groups.id'), nullable=False)
+    amount = Column(Integer, nullable=False)
