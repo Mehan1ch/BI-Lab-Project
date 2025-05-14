e25ee324fc036db6c36cfb891db10d3547d01bf2
Péter Hanich <hanipet@gmail.com>
2025-05-14 11:51:27 +0200

    feat: implement countries model

---
+++ etl/load/models/countries.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Country(Base):
+    __tablename__ = 'countries'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    total_populations = Column(Integer, nullable=False)
