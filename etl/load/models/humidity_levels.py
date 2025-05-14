76f5c56b5f339369ee0050687bf7ce91a2758c0e
Péter Hanich <hanipet@gmail.com>
2025-05-14 12:15:38 +0200

    feat: implement humidity_levels model

---
+++ etl/load/models/humidity_levels.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class HumidityLevel(Base):
+    __tablename__ = 'humidity_levels'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    range_start = Column(Float, nullable=True)
+    range_end = Column(Float, nullable=True)
